2efe675babc0b8e075717e7ba424fbb404dee29f
nicetoseeyou <hello4world@yeah.net>
2019-10-23 00:07:49 +0800

    add bundle info entity

---
+++ nice-nifi-kafka-bundle/nice-nifi-kafka-processor/src/main/java/lab/nice/nifi/processor/kafka/BundleInfo.java
@@ -0,0 +1,59 @@
+package lab.nice.nifi.processor.kafka;
+
+import org.apache.kafka.common.TopicPartition;
+import org.apache.nifi.serialization.record.RecordSchema;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class BundleInfo {
+    private final TopicPartition topicPartition;
+    private final RecordSchema schema;
+    private final Map<String, String> attributes;
+
+    public BundleInfo(TopicPartition topicPartition, RecordSchema schema, Map<String, String> attributes) {
+        this.topicPartition = topicPartition;
+        this.schema = schema;
+        this.attributes = attributes;
+    }
+
+    public TopicPartition getTopicPartition() {
+        return topicPartition;
+    }
+
+    public RecordSchema getSchema() {
+        return schema;
+    }
+
+    public Map<String, String> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final BundleInfo that = (BundleInfo) o;
+        return Objects.equals(topicPartition, that.topicPartition) &&
+                Objects.equals(schema, that.schema) &&
+                Objects.equals(attributes, that.attributes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(topicPartition, schema, attributes);
+    }
+
+    @Override
+    public String toString() {
+        return "BundleInfo{" +
+                "topicPartition=" + topicPartition +
+                ", schema=" + schema +
+                ", attributes=" + attributes +
+                '}';
+    }
+}
